522316612b85c8af22d3dff3679f849e0e032e3e
camikei <3121414@stud.srh-campus-berlin.de>
2024-04-11 20:44:57 +0200

    all js done

--- js-tasks/working-with-objects.js
+++ js-tasks/working-with-objects.js
@@ -4,13 +4,25 @@ const library = [
     {title: 'Mockingjay', author: 'Suzanne Collins', year: 2010, bestseller: false},
     {title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008, bestseller: true}
 ]
-
 // Log the result after each task:
 
 // List each author (without duplicates)...
 
 // List all book titles published after 2000...
 
+const booksAfter2000=library.filter((books)=>books.year>2000)
+console.log(booksAfter2000)
+
 // List all property names of the first book separated by a comma (expected output: 'title, author, year')...
 
+const propNames = []
+library.forEach(function (o){
+    Object.keys(o).forEach(function (prop){
+        if (propNames.indexOf(prop)< 0)
+            propNames.push(prop)
+    })
+})
+console.log(propNames)
 // List all bestseller book titles...
+
+const bestseller = library.filter((bestsellers)=>bestsellers.title)
